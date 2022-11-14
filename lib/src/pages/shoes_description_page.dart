dd46dcd7e6a87af36db35a2ffcb7ba34c6882359
ddienu <rockdann@hotmail.com>
2022-11-14 00:49:14 -0500

    Update: shoes description page.
    Update shoes description page implementing the price and the buy button in the column of the single child scroll view.

--- lib/src/pages/shoes_description_page.dart
+++ lib/src/pages/shoes_description_page.dart
@@ -13,16 +13,55 @@ class ShoesDescriptionPage extends StatelessWidget {
         Expanded(
             child: SingleChildScrollView(
           child: Column(
-            children: const [
+            children: [
               ZapatoDescripcion(
                 titulo: 'Nike Air Max 720',
                 descripcion:
                     "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
               ),
+              SizedBox(height: 20),
+
+              _MontoBuyNow(),
             ],
           ),
         )),
       ],
     ));
+  }
+}
+
+class _MontoBuyNow extends StatelessWidget {
+  const _MontoBuyNow({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.symmetric( horizontal: 20),
+        child: Row(
+          children: [
+            Text('\$',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, 
+                    fontSize: 13, 
+                    height: 2)),
+
+            Text('180.0',
+                style: TextStyle(fontWeight: FontWeight.bold, 
+                fontSize: 20
+                )),
+
+            OrangeButton(
+              texto: 'Buy now', 
+              buttonWidth: 110, 
+              buttonHeight: 45,
+              buttonColor: Colors.orange
+              )
+          ],
+        ),
+      ),
+    );
   }
 }
